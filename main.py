69c08405a559ee000877be7dbfeb6d7ad682f3ce
Guilherme Somera <guisomera22@gmail.com>
2026-05-08 09:42:17 -0300

    docs: add project documentation and diagrams

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import llm
 import database
 
 async def ask_question():
-    question = input("Qual informação voce deseja saber ?")
+    question = input("Qual informação voce deseja saber ?\n")
     llm_response = llm.create_sql(user_request=question)
 
     pool = await database.open_pool()
     database_response = await database.execuete_command(pool, llm_response)
 
     format_result = llm.format_result(question, database_response)
-    print(format_result)
+    print(f"\n{format_result} \n")
 
 asyncio.run(ask_question())
